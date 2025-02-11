24e307bc916756a93996639a2125bf41e0428449
gamer879 <aassoftwaresolution@gmail.com>
2025-02-12 00:59:55 +0500

    Create counter.js

---
+++ counter.js
@@ -0,0 +1,13 @@
+
+
+
+document.write('');
+document.write('<script async src="https://www.googletagmanager.com/gtag/js?id=G-S9JJ3929MK"></script>');
+document.write('<script>');
+document.write('  window.dataLayer = window.dataLayer || [];');
+document.write('  function gtag(){dataLayer.push(arguments);}');
+document.write('  gtag(\'js\', new Date());');
+document.write('');
+document.write('  gtag(\'config\', \'G-S9JJ3929MK\');');
+document.write('</script>');
+
