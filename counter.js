59bc30e8bf31d8c2d43c417a71b9641ca125db8f
gamer879 <aassoftwaresolution@gmail.com>
2025-02-12 01:09:59 +0500

    Update counter.js

--- counter.js
+++ counter.js
@@ -1,13 +1,15 @@
+// Load Google Analytics asynchronously
+var script1 = document.createElement("script");
+script1.async = true;
+script1.src = "https://www.googletagmanager.com/gtag/js?id=G-S9JJ3929MK";
+document.head.appendChild(script1);
 
-
-
-document.write('');
-document.write('<script async src="https://www.googletagmanager.com/gtag/js?id=G-S9JJ3929MK"></script>');
-document.write('<script>');
-document.write('  window.dataLayer = window.dataLayer || [];');
-document.write('  function gtag(){dataLayer.push(arguments);}');
-document.write('  gtag(\'js\', new Date());');
-document.write('');
-document.write('  gtag(\'config\', \'G-S9JJ3929MK\');');
-document.write('</script>');
-
+// Initialize Google Analytics
+script1.onload = function () {
+    window.dataLayer = window.dataLayer || [];
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+    gtag("js", new Date());
+    gtag("config", "G-S9JJ3929MK");
+};
